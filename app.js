8e46da3b2d1fe773ef5622b4ad2f9c4760fbd232
Taichi Iida <chiaki.621@gmail.com>
2022-08-01 17:35:15 +0900

    一本通す

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || '5000'
 const bodyParser = require('body-parser');
 
+app.use(cors())
 // urlencodedとjsonは別々に初期化する
 app.use(bodyParser.urlencoded({
   extended: true
@@ -35,6 +37,16 @@ app.post('/test', async (req, res) => {
   });
 
   const saved_test_data = await test_data.save();
+  res.json(saved_test_data);
+
+})
+
+app.delete('/test', async (req, res) => {
+  const test_data = new test({
+    _id: req.body._id,
+  });
+
+  const saved_test_data = await test_data.delete();
   res.json(saved_test_data);
 
 })
