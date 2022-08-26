5aad0ba29c5c90e61523fa9bbbe73cace707a066
Taichi Iida <chiaki.621@gmail.com>
2022-08-26 13:04:51 +0900

    カラム増加OK

--- app.js
+++ app.js
@@ -19,37 +19,70 @@ db.on('error', console.error.bind(console, 'DB connection error:'));
 db.once('open', () => console.log('DB connection successful'));
 
 
-const test = require('./db_modules/test');
+
+require('./db_modules/test').then(test => {
+
+  app.get('/test', async (req, res) => {
+    const test_data = await test.find({});
+    res.json(test_data);
+  })
+
+  app.post('/test', async (req, res) => {
+    const test_data = new test({
+      name: req.body.name,
+      str: req.body.str
+    });
+
+    const saved_test_data = await test_data.save();
+    res.json(saved_test_data);
+
+  })
+
+  app.delete('/test', async (req, res) => {
+    const test_data = new test({
+      _id: req.body._id,
+    });
+
+    const saved_test_data = await test_data.delete();
+    res.json(saved_test_data);
+
+  })
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/test', async (req, res) => {
-  const test_data = await test.find({});
-  res.json(test_data);
-})
 
-app.post('/test', async (req, res) => {
-  const test_data = new test({
-    name: req.body.name,
-    str: req.body.str
+const schema_setting = require('./db_modules/setting/schema_setting');
+
+app.get('/schema_setting/:schema_name', async (req, res) => {
+  const schema_setting_data = await schema_setting.find({
+    schema_name: req.params.schema_name
   });
+  res.json(schema_setting_data);
+})
 
-  const saved_test_data = await test_data.save();
-  res.json(saved_test_data);
+app.post('/schema_setting', async (req, res) => {
+  let data = req.body;
+  const schema_setting_data = new schema_setting(data);
+
+  const saved_schema_setting_data = await schema_setting_data.save();
+  res.json(saved_schema_setting_data);
 
 })
 
-app.delete('/test', async (req, res) => {
-  const test_data = new test({
-    _id: req.body._id,
-  });
 
-  const saved_test_data = await test_data.delete();
-  res.json(saved_test_data);
 
-})
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- db_modules/test.js
+++ db_modules/test.js
@@ -1,9 +1,30 @@
 
+const DBConnection = require('../connection/DBConnection')
+const DBName = 'test'
+const con = DBConnection(DBName);
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const schema_name = 'test'
 
-const TestSchema = mongoose.Schema({
-    name: String,
-    str: String
-});
+const schema_setting = require('./setting/schema_setting');
 
-module.exports = mongoose.model('Test', TestSchema);
+module.exports = new Promise((resolve, reject) => {
+    schema_setting.findOne({
+        schema_name: schema_name
+    }, (error, data) => {
+
+        if (error) {
+            reject(error)
+        }
+
+        let schema = {};
+        const columns = data.columns;
+        for (let i = 0; i < columns.length; i++) {
+            const column = columns[i];
+            schema[column.column_name] = column.type
+        }
+        let TestSchema = new Schema(schema);
+        const testObj = con.model('Test', TestSchema);
+        resolve(testObj);
+    })
+})
