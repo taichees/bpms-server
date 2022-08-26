3a5463fef394bfea2f63f7770de5e9730da94226
Taichi Iida <chiaki.621@gmail.com>
2022-08-26 17:22:14 +0900

    より汎用化

--- app.js
+++ app.js
@@ -11,51 +11,25 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose');
-const DB = process.env.DB_URL || 'mongodb://127.0.0.1/test_db';
-mongoose.connect(DB);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'DB connection error:'));
-db.once('open', () => console.log('DB connection successful'));
-
-
-
-require('./db_modules/test').then(test => {
-
-  app.get('/test', async (req, res) => {
-    const test_data = await test.find({});
-    res.json(test_data);
-  })
-
-  app.post('/test', async (req, res) => {
-    const test_data = new test({
-      name: req.body.name,
-      str: req.body.str
-    });
-
-    const saved_test_data = await test_data.save();
-    res.json(saved_test_data);
-
-  })
-
-  app.delete('/test', async (req, res) => {
-    const test_data = new test({
-      _id: req.body._id,
-    });
-
-    const saved_test_data = await test_data.delete();
-    res.json(saved_test_data);
-
-  })
-
-})
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
 const schema_setting = require('./db_modules/setting/schema_setting');
+const createModel = require('./db_modules/createModel');
+// 登録
+app.post('/schema_setting', async (req, res) => {
+  let data = req.body;
+  const schema_setting_data = new schema_setting(data);
+  const saved_schema_setting_data = await schema_setting_data.save();
+  res.json(saved_schema_setting_data);
+})
+
+// 取得
+app.get('/schema_setting', async (req, res) => {
+  const schema_setting_data = await schema_setting.find({});
+  res.json(schema_setting_data);
+})
 
 app.get('/schema_setting/:schema_name', async (req, res) => {
   const schema_setting_data = await schema_setting.find({
@@ -64,25 +38,62 @@ app.get('/schema_setting/:schema_name', async (req, res) => {
   res.json(schema_setting_data);
 })
 
-app.post('/schema_setting', async (req, res) => {
-  let data = req.body;
-  const schema_setting_data = new schema_setting(data);
-
-  const saved_schema_setting_data = await schema_setting_data.save();
-  res.json(saved_schema_setting_data);
-
+// 更新
+app.put('/schema_setting/:id', async (req, res) => {
+  const schema_setting_data = await schema_setting.findByIdAndUpdate(req.params.id, req.body);
+  // 変更前の情報が返る
+  createModel.delete(schema_setting_data);
+  res.json(schema_setting_data);
 })
 
+// 削除
+app.delete('/schema_setting/:id', async (req, res) => {
+  const schema_setting_data = await schema_setting.findByIdAndDelete(req.params.id);
+  // 変更前の情報が返る
+  createModel.delete(schema_setting_data);
+  res.json(schema_setting_data);
+})
 
+// モデル取得
+const getModel = async function (schema_name) {
+  let data = await schema_setting.findOne({ schema_name: schema_name })
+  return createModel.get(data);
+}
 
+// 登録
+app.post('/schema/:schema_name', async (req, res) => {
+  let model = await getModel(req.params.schema_name)
+  const model_data = new model(req.body);
+  const saved_model_data = await model_data.save();
+  model.remove({});
+  res.json(saved_model_data);
+})
 
+// 取得
+app.get('/schema/:schema_name', async (req, res) => {
+  let model = await getModel(req.params.schema_name)
+  const model_data = await model.find({});
+  model.remove({});
+  res.json(model_data);
+})
 
+// 更新
+app.put('/schema/:schema_name/:id', async (req, res) => {
+  let model = await getModel(req.params.schema_name)
+  // 変更前の情報が返る
+  const model_data = await model.findByIdAndUpdate(req.params.id, req.body);
+  model.remove({});
+  res.json(model_data);
+})
 
-
-
-
-
-
+// 削除
+app.delete('/schema/:schema_name/:id', async (req, res) => {
+  let model = await getModel(req.params.schema_name)
+  // 変更前の情報が返る
+  const model_data = await model.findByIdAndDelete(req.params.id);
+  model.remove({});
+  res.json(model_data);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
